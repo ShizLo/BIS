e615dd4fb6d9923190f867e7f5cd11dac543d531
Your Name <you@example.com>
2024-11-25 20:01:57 +0300

    first commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,130 @@
+<template>
+  <section class="banner-wrapper">
+    <v-container class="banner-block">
+      <v-row class="text-center">
+        <v-col cols="6 d-flex">
+          <div class="banner__block-text text-left">
+            <h1 class="banner__text">Инженерные коммуникации и благоустройство участка</h1>
+            <button class="banner__button">
+              Обсудить задачу
+              <svg
+                class="banner__button-icon"
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="Frame 1864">
+                  <path
+                    id="Vector"
+                    d="M1.75 7.29163H12.25M12.25 7.29163L7 12.5416M12.25 7.29163L7 2.04163"
+                    stroke="#102938"
+                    stroke-width="1.4"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </g>
+              </svg>
+            </button>
+          </div>
+        </v-col>
+        <v-col cols="6">
+          <v-img :src="logo" class="my-3" contain height="200" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<script>
+import logo from '../assets/logo.svg'
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ],
+    importantLinks: [
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    logo,
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Roadmap',
+        href: 'https://vuetifyjs.com/introduction/roadmap/',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
+}
+</script>
+
+<style>
+.banner-wrapper {
+  background-color: #f5f5f5;
+}
+.banner__block-text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 60px;
+}
+.banner__text {
+  font-weight: 700;
+  line-height: 120%;
+  font-size: 34px;
+  color: #102938;
+}
+.banner__button {
+  font: 600 16px / 125% 'Roboto Flex';
+  display: flex;
+  height: 48px;
+  padding: 11px 14px;
+  align-items: center;
+  gap: 8px;
+  border-radius: 4px;
+  border: 1px solid #102938;
+  background: rgba(245, 245, 245, 0);
+  color: #102938;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.3s ease 0s;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,305 @@
+<template>
+  <Banner />
+  <section class="content">
+    <v-container>
+      <h2 class="content__text">Наши услуги</h2>
+      <div class="content__services"></div>
+      <v-row>
+        <v-col cols="6">
+          <v-row>
+            <v-col cols="12"
+              ><div class="content_item_1">
+                <div class="content__item-wrap">
+                  <a class="content__link">
+                    <div class="content__description">
+                      <div class="content__title">Монтаж ЛОС</div>
+                      <div class="content__price">
+                        <span>От 65 000 руб.</span>
+                      </div>
+                    </div>
+                    <div class="content__plus">
+                      <div class="plus__item">
+                        <svg
+                          class="plus__item-icon"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            id="Vector"
+                            d="M13 10V3L5 14H11V21L19 10H13Z"
+                            stroke="#102938"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </svg>
+                        <span class="plus__item-text">Быстрый монтаж</span>
+                      </div>
+
+                      <div class="plus__item">
+                        <svg
+                          class="content-bar__icon"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            id="Vector"
+                            d="M7 20V9.00001M16.472 20H4.1C3.94087 20 3.78826 19.9368 3.67574 19.8243C3.56321 19.7118 3.5 19.5591 3.5 19.4V9.60001C3.5 9.44088 3.56321 9.28826 3.67574 9.17574C3.78826 9.06322 3.94087 9.00001 4.1 9.00001H6.868C7.21337 9.00001 7.55286 8.91058 7.8534 8.74041C8.15395 8.57025 8.40531 8.32516 8.583 8.02901L11.293 3.51201C11.4974 3.17086 11.8194 2.91614 12.1984 2.79576C12.5774 2.67538 12.9874 2.69763 13.3512 2.85832C13.7149 3.01902 14.0075 3.30708 14.1737 3.66835C14.34 4.02961 14.3685 4.43917 14.254 4.82001L13.232 8.22801C13.2052 8.31752 13.1997 8.41204 13.2159 8.50406C13.2321 8.59607 13.2696 8.68303 13.3253 8.75801C13.3811 8.83298 13.4536 8.89391 13.537 8.93592C13.6205 8.97793 13.7126 8.99988 13.806 9.00001H18.381C18.6885 8.99994 18.992 9.07081 19.2677 9.20708C19.5434 9.34336 19.7839 9.54139 19.9706 9.78576C20.1573 10.0301 20.2852 10.3143 20.3442 10.6161C20.4033 10.9179 20.3919 11.2293 20.311 11.526L18.401 18.526C18.2857 18.9491 18.0345 19.3225 17.6861 19.5889C17.3378 19.8552 16.9105 19.9997 16.472 20Z"
+                            stroke="#102938"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </svg>
+                        <span class="plus__item-text">Год гарантии</span>
+                      </div>
+
+                      <div class="plus__item">
+                        <svg
+                          class="content-bar__icon"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            id="Vector"
+                            d="M7 20V9.00001M16.472 20H4.1C3.94087 20 3.78826 19.9368 3.67574 19.8243C3.56321 19.7118 3.5 19.5591 3.5 19.4V9.60001C3.5 9.44088 3.56321 9.28826 3.67574 9.17574C3.78826 9.06322 3.94087 9.00001 4.1 9.00001H6.868C7.21337 9.00001 7.55286 8.91058 7.8534 8.74041C8.15395 8.57025 8.40531 8.32516 8.583 8.02901L11.293 3.51201C11.4974 3.17086 11.8194 2.91614 12.1984 2.79576C12.5774 2.67538 12.9874 2.69763 13.3512 2.85832C13.7149 3.01902 14.0075 3.30708 14.1737 3.66835C14.34 4.02961 14.3685 4.43917 14.254 4.82001L13.232 8.22801C13.2052 8.31752 13.1997 8.41204 13.2159 8.50406C13.2321 8.59607 13.2696 8.68303 13.3253 8.75801C13.3811 8.83298 13.4536 8.89391 13.537 8.93592C13.6205 8.97793 13.7126 8.99988 13.806 9.00001H18.381C18.6885 8.99994 18.992 9.07081 19.2677 9.20708C19.5434 9.34336 19.7839 9.54139 19.9706 9.78576C20.1573 10.0301 20.2852 10.3143 20.3442 10.6161C20.4033 10.9179 20.3919 11.2293 20.311 11.526L18.401 18.526C18.2857 18.9491 18.0345 19.3225 17.6861 19.5889C17.3378 19.8552 16.9105 19.9997 16.472 20Z"
+                            stroke="#102938"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </svg>
+                        <span class="plus__item-text">Работа под ключ</span>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div></v-col
+            >
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <div class="content_item_2">
+                <div class="content__item-wrap content__item-wrap-sm">
+                  <a class="content__link">
+                    <div class="content__description">
+                      <div class="content__title">Водоснабжение</div>
+                      <div class="content__price">
+                        <span>От 55 000 руб.</span>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </v-col>
+            <v-col cols="6">
+              <div class="content_item_2">
+                <div class="content__item-wrap content__item-wrap-sm">
+                  <a class="content__link">
+                    <div class="content__description">
+                      <div class="content__title">Дренаж</div>
+                      <div class="content__price">
+                        <span>От 80 000 руб.</span>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="6">
+          <v-row>
+            <v-col cols="6">
+              <div class="content_item_2">
+                <div class="content__item-wrap content__item-wrap-sm">
+                  <a class="content__link">
+                    <div class="content__description">
+                      <div class="content__title">Благоустройство участка</div>
+                      <div class="content__price">
+                        <span>От 15 000 руб.</span>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </v-col>
+            <v-col cols="6">
+              <div class="content_item_2">
+                <div class="content__item-wrap content__item-wrap-sm">
+                  <a class="content__link">
+                    <div class="content__description">
+                      <div class="content__title">Заборы</div>
+                      <div class="content__price">
+                        <span>От 55 000 руб.</span>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12"
+              ><div class="content_item_1">
+                <div class="content__item-wrap">
+                  <a class="content__link">
+                    <div class="content__description">
+                      <div class="content__title">Свайное поле</div>
+                      <div class="content__price">
+                        <span>От 108 000 руб.</span>
+                      </div>
+                    </div>
+                    <div class="content__plus">
+                      <div class="plus__item">
+                        <svg
+                          class="plus__item-icon"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            id="Vector"
+                            d="M13 10V3L5 14H11V21L19 10H13Z"
+                            stroke="#102938"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </svg>
+                        <span class="plus__item-text">Быстрый монтаж</span>
+                      </div>
+
+                      <div class="plus__item">
+                        <svg
+                          class="content-bar__icon"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            id="Vector"
+                            d="M7 20V9.00001M16.472 20H4.1C3.94087 20 3.78826 19.9368 3.67574 19.8243C3.56321 19.7118 3.5 19.5591 3.5 19.4V9.60001C3.5 9.44088 3.56321 9.28826 3.67574 9.17574C3.78826 9.06322 3.94087 9.00001 4.1 9.00001H6.868C7.21337 9.00001 7.55286 8.91058 7.8534 8.74041C8.15395 8.57025 8.40531 8.32516 8.583 8.02901L11.293 3.51201C11.4974 3.17086 11.8194 2.91614 12.1984 2.79576C12.5774 2.67538 12.9874 2.69763 13.3512 2.85832C13.7149 3.01902 14.0075 3.30708 14.1737 3.66835C14.34 4.02961 14.3685 4.43917 14.254 4.82001L13.232 8.22801C13.2052 8.31752 13.1997 8.41204 13.2159 8.50406C13.2321 8.59607 13.2696 8.68303 13.3253 8.75801C13.3811 8.83298 13.4536 8.89391 13.537 8.93592C13.6205 8.97793 13.7126 8.99988 13.806 9.00001H18.381C18.6885 8.99994 18.992 9.07081 19.2677 9.20708C19.5434 9.34336 19.7839 9.54139 19.9706 9.78576C20.1573 10.0301 20.2852 10.3143 20.3442 10.6161C20.4033 10.9179 20.3919 11.2293 20.311 11.526L18.401 18.526C18.2857 18.9491 18.0345 19.3225 17.6861 19.5889C17.3378 19.8552 16.9105 19.9997 16.472 20Z"
+                            stroke="#102938"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </svg>
+                        <span class="plus__item-text">Гарантия качества</span>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div></v-col
+            >
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+// Components
+import Banner from '../components/Banner.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    Banner,
+  },
+})
+</script>
+
+<style>
+.content {
+  padding: 45px 0;
+}
+.content__text {
+  font-weight: 600;
+  font-size: 1.8rem;
+  color: #102938;
+  letter-spacing: 0.28px;
+  margin-bottom: 30px;
+}
+.content_item_1 {
+  height: 350px;
+}
+.content_item_2 {
+  height: 220px;
+}
+.content__item-wrap {
+  background-color: #f5f5f5;
+  display: flex;
+  height: 340px;
+  border-radius: 7px;
+  transition: background-color 0.2s ease-in-out;
+}
+.content__item-wrap-sm {
+  height: 220px;
+}
+.content__link {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 100px;
+  width: 100%;
+  height: auto;
+  padding: 20px 20px 40px;
+}
+.content__title {
+  font-weight: 700;
+  font-size: 1.55rem;
+  /* font-family: 'Roboto Flex'; */
+  color: #102938;
+  padding-bottom: 20px;
+}
+.content__price {
+  font: 600 16px / 125% 'Roboto Flex';
+  background: #fff;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 6px;
+  padding: 11px 12px;
+  color: #102938;
+  text-align: center;
+  border-radius: 4px;
+}
+.content__plus {
+  max-width: 245px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 8px;
+}
+.plus__item {
+  display: inline-flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 8px;
+}
+.plus__item-text {
+  font: 500 16px / 125% 'Roboto Flex';
+  color: #102938;
+}
+</style>
